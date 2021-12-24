aef0197b6c68d8326afad85574ef00e8f1a02194
nima heidari <nima.heidari.n@gmail.com>
2021-12-25 00:48:53 +0330

    add middleware controll browser & hisorange/ bro_d

--- App/Middlewares/BlockIEMiddleware.php
+++ App/Middlewares/BlockIEMiddleware.php
@@ -2,12 +2,12 @@
 namespace App\Middlewares;
 
 use App\Middlewares\contract\MiddlewareInterface;
-
+use hisorange\BrowserDetect\Parser as Browser;
 class BlockIEMiddleware implements MiddlewareInterface{
     public function handle()
     {
-        global $request;
-       //var_dump($request);
-        echo 1;
+       if(Browser::isIE()){
+           die("IE is blocked");
+       }
     }
 }
