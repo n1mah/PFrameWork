92351e9ff54559c094cdf4c4992ec6049773004e
nima heidari <nima.heidari.n@gmail.com>
2021-12-24 09:34:52 +0330

    add bootstrap init

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
-use  App\Core\Request;
-include "vendor/autoload.php";
+include "bootstrap/init.php";
 
-echo $_SERVER["REQUEST_URI"];
-echo "<br>";
-$r=new Request();
-echo "<br>";
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-echo "<pre>";
-print_r($_ENV);
-echo "/<pre>";
+
+var_dump($_ENV);
+
+
