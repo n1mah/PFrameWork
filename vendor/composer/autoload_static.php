e5abe2c7e65f861c6aefe5f176d40186df547258
nima heidari <nima.heidari.n@gmail.com>
2021-12-25 00:49:32 +0330

    add middleware & controll browser

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,12 +7,21 @@ namespace Composer\Autoload;
 class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
 {
     public static $files = array (
+        '04c6c5c2f7095ccf6c481d3e53e1776f' => __DIR__ . '/..' . '/mustangostang/spyc/Spyc.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'h' => 
+        array (
+            'hisorange\\BrowserDetect\\' => 24,
+        ),
+        'U' => 
+        array (
+            'UAParser\\' => 9,
+        ),
         'S' => 
         array (
             'Symfony\\Polyfill\\Php80\\' => 23,
@@ -23,6 +32,14 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
         array (
             'PhpOption\\' => 10,
         ),
+        'L' => 
+        array (
+            'League\\Pipeline\\' => 16,
+        ),
+        'J' => 
+        array (
+            'Jaybizzle\\CrawlerDetect\\' => 24,
+        ),
         'G' => 
         array (
             'GrahamCampbell\\ResultType\\' => 26,
@@ -30,6 +47,11 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
         'D' => 
         array (
             'Dotenv\\' => 7,
+            'DeviceDetector\\' => 15,
+        ),
+        'C' => 
+        array (
+            'Composer\\CaBundle\\' => 18,
         ),
         'A' => 
         array (
@@ -38,6 +60,14 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
     );
 
     public static $prefixDirsPsr4 = array (
+        'hisorange\\BrowserDetect\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hisorange/browser-detect/src',
+        ),
+        'UAParser\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/ua-parser/uap-php/src',
+        ),
         'Symfony\\Polyfill\\Php80\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
@@ -54,6 +84,14 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
         array (
             0 => __DIR__ . '/..' . '/phpoption/phpoption/src/PhpOption',
         ),
+        'League\\Pipeline\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/league/pipeline/src',
+        ),
+        'Jaybizzle\\CrawlerDetect\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/jaybizzle/crawler-detect/src',
+        ),
         'GrahamCampbell\\ResultType\\' => 
         array (
             0 => __DIR__ . '/..' . '/graham-campbell/result-type/src',
@@ -62,15 +100,34 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
         array (
             0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
         ),
+        'DeviceDetector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/matomo/device-detector',
+        ),
+        'Composer\\CaBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/ca-bundle/src',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/App',
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'D' => 
+        array (
+            'Detection' => 
+            array (
+                0 => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/namespaced',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Mobile_Detect' => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/Mobile_Detect.php',
         'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
         'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
         'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
@@ -81,6 +138,7 @@ class ComposerStaticInit066bd7f20db0046483926c9ee2c7888d
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit066bd7f20db0046483926c9ee2c7888d::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit066bd7f20db0046483926c9ee2c7888d::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit066bd7f20db0046483926c9ee2c7888d::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit066bd7f20db0046483926c9ee2c7888d::$classMap;
 
         }, null, ClassLoader::class);
